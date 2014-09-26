7e846088d96ad686802fc18169536ba63b83b4d9
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2014-09-26 14:33:38 +0200

    Corrected potential problem in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,10 +40,6 @@ module.exports = function(grunt) {
         files: cssfiles,
         tasks: ['cssmin'],
       },
-      js: {
-        files: jsfiles,
-        tasks: ['uglify'],
-      },
     },
     cssmin: {
       dist: {
